700bc88abe3f53945235bf419bdd2e396c02cba0
Matias Hernandez <matias2k00@gmail.com>
2024-01-17 23:23:04 -0300

    repasando practicas ts

---
+++ typescript-intro/src/topics/01-object-interface.ts
@@ -0,0 +1,13 @@
+let skills = ['Bash', 'Counter', 'Healing', true];
+
+interface character {
+  name: string;
+  hp: number;
+  skills: string[];
+}
+
+const strider: character = {
+  name: 'Matias',
+  hp: 100,
+  skills: ['Bash', 'Counter'],
+};
