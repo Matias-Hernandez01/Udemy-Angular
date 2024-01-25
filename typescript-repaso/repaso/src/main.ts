e40f483d564f984e1075f716771baa3bf69ced96
Matias Hernandez <matias2k00@gmail.com>
2024-01-24 23:59:18 -0300

    repaso

--- typescript-repaso/repaso/src/main.ts
+++ typescript-repaso/repaso/src/main.ts
@@ -1,12 +1,9 @@
 import './style.css';
-import { setupCounter } from './counter.ts';
 
-import './topics/01-functions.ts';
+import './topics/05-clases';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Hola mundo</h1>
   </div>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
