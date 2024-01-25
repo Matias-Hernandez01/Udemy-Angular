ae6cd0386567010ada07513855684a6db41bdced
Matias Hernandez <matias2k00@gmail.com>
2024-01-24 23:47:50 -0300

    functions ts

--- typescript-repaso/repaso/src/topics/01-functions.ts
+++ typescript-repaso/repaso/src/topics/01-functions.ts
@@ -1,3 +1,27 @@
-function addNumber() {
-  return;
+// function addNumber(a: number, b: number) {
+// return a + b;
+// }
+// const results = addNumber(2, 3);
+// console.log(results);
+
+interface Character {
+  name: string;
+  pv: number;
+  showHp: () => void;
 }
+
+const healCharacter = (character: Character, amount: number) => {
+  return (character.pv += amount);
+};
+
+const strider: Character = {
+  name: 'Strider',
+  pv: 50,
+  showHp() {
+    console.log('Puntos de vida' + this.pv);
+  },
+};
+
+const results = healCharacter(strider, 60);
+
+console.log(results);
